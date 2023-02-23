b3c68501819ae7c2af402e5a646582ad162bc5da
Julian Clatro <jclatro@yahoo.com>
2023-02-23 15:38:50 -0300

    0.0.0-b

--- app/routes/save.tsx
+++ app/routes/save.tsx
@@ -5,6 +5,6 @@ import { User } from "~/models/User";
 export async function loader(args: LoaderArgs) {
   const { DB } = args.context.env as any
   console.log('DB', DB)
-  // await User.create(new User({ name: 'Charles' }), DB)
+  await User.create(new User({ name: 'Charles' }), DB)
   return json({ name: "Ryan", date: new Date() });
 }
