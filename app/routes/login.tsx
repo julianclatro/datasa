0a34a26d8842903490d76c0ecd5d0eb4d93ea798
Julian Clatro <jclatro@yahoo.com>
2023-04-27 16:10:58 -0300

    update to login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -5,22 +5,31 @@ export default function Login() {
   const fetcher = useFetcher();
 
   React.useEffect(() => {
-    if (fetcher.state === 'idle') {
-      console.log(fetcher.data)
+    if (fetcher.state === "idle") {
+      console.log(fetcher.data);
     }
   }, [fetcher]);
   return (
     <div className="w-[540px] mx-auto">
-      <h1 className="text-lg font-bold">Nueva Sesión</h1>
-      <fetcher.Form method="post" action="/sessions/new">
-        <div className="flex flex-col space-y-4">
-          <label>Usuario</label>
-          <input type="text" name="username" id="username"/>
-          <label>Contraseña</label>
-          <input type="password" name="password" id="password"/>
-          <button type="submit" className="px-[30px] py-[15px] bg-slate-900 text-white">Conectar</button>
+      <div className="flex items-center h-screen">
+        <div className="w-full py-[20px] px-[40px] bg-green-200 rounded-3xl">
+          <h1 className="text-h3-normal-bold font-bold">DATASA ♻️</h1>
+          <fetcher.Form method="post" action="/sessions/new">
+            <div className="flex flex-col space-y-4">
+              <label>Usuario</label>
+              <input type="text" name="username" id="username" className="rounded-lg" />
+              <label>Contraseña</label>
+              <input type="password" name="password" id="password" className="rounded-lg"/>
+              <button
+                type="submit"
+                className="px-[30px] py-[15px] bg-green-600 text-white rounded-xl"
+              >
+                Conectar
+              </button>
+            </div>
+          </fetcher.Form>
         </div>
-      </fetcher.Form>
+      </div>
     </div>
   );
-} 
+}
